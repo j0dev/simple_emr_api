26919aef3648068019e823cd6146e43fc17443f7
j0dev <rig0408@naver.com>
2021-12-26 07:51:10 +0900

    1. Delete unused code 2. Write README.md

--- emr/urls.py
+++ emr/urls.py
@@ -17,4 +17,16 @@ urlpatterns = [
     path('visit/race/', VisitRaceView.as_view()),
     path('visit/ethnicity/', VisitEthnicityView.as_view()),
     path('visit/age/', VisitAgeGroupView.as_view()),
+
+    # concept 관련
+    # concept의 domain_id 정보를 받아오는 url
+    # concetp의 search 없이 보는 api (pagination) -> 정렬 기준 : concept_id 기준
+    # concetp의 search가 가능한 api (pagination)
+
+    # table row 관련
+    # person 전체 read
+    # visit_occurrence 전체 read
+    # condition_occurrence 전체 read
+    # drug_exposure 전체 read
+    #
 ]
--- emr/views/visit/visitCount.py
+++ emr/views/visit/visitCount.py
@@ -7,8 +7,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from emr.models import VisitOccurrence, Person
-from datetime import datetime, timedelta
+from emr.models import VisitOccurrence
 
 
 class VisitTypeView(APIView):
