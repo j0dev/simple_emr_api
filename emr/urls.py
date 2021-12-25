367fa7cde1f15cb64574923fb394ae3863ca957b
j0dev <rig0408@naver.com>
2021-12-26 05:30:27 +0900

    [Implement API : Get some number of visit information] 1. Get the number of visitors type - param : get - url : "~/api/visit/type/" - response : {"Emergency Room Visit": : $result, ...}
    2. Get the number of all visitors gender
    - param : get
    - "~/api/visit/gender/"
    - response : {"FEMALE" : $result, "MALE", $result}
    
    3. Get the number of all visitors race
    - param : get
    - "~/api/visit/race/"
    - response : {"White" : $result, "Asian" : $result, ...}
    
    4. Get the number of all visitors ethnicity
    - param : get
    - "~/api/visit/ethnicity/"
    - response : {"???" : ???}
    - ## no testing data in ethnicity (it's only 0 exist)
    
    [Change Urls.py]
    - simple_emr_api/urls.py -> just handle the url "api" or not
    - emr/urls.py -> handle all "api" url
    
    [Remove Unused model]
    - delete emr/models.py

--- emr/urls.py
+++ emr/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from emr.views.patient.patientCount import PatientAllView, PatientGenderView, PatientRaceView, PatientEthnicityView, PatientDeathView
+from emr.views.visit.visitCount import VisitTypeView, VisitGenderView, VisitRaceView, VisitEthnicityView
 
 
 urlpatterns = [
-    path('all/', PatientAllView.as_view()),
-    path('gender/', PatientGenderView.as_view()),
-    path('race/', PatientRaceView.as_view()),
-    path('ethnicity/', PatientEthnicityView.as_view()),
-    path('death/', PatientDeathView.as_view()),
+    # 환자 관련
+    path('patient/all/', PatientAllView.as_view()),
+    path('patient/gender/', PatientGenderView.as_view()),
+    path('patient/race/', PatientRaceView.as_view()),
+    path('patient/ethnicity/', PatientEthnicityView.as_view()),
+    path('patient/death/', PatientDeathView.as_view()),
+
+    # 방문 관련
+    path('visit/type/', VisitTypeView.as_view()),
+    path('visit/gender/', VisitGenderView.as_view()),
+    path('visit/race/', VisitRaceView.as_view()),
+    path('visit/ethnicity/', VisitEthnicityView.as_view()),
 ]
